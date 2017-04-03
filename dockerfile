9f6fdc893cd94d11d25a85a2ced2e6d869b63c4d
mattdparker <matthewdanielparker@gmail.com>
2017-04-03 10:14:17 +0100

    templates need installing - adding manifest, setupa nd install to dockerfile

--- dockerfile
+++ dockerfile
@@ -9,6 +9,8 @@ WORKDIR /tmp/helloworld/
 RUN pip install -U pip
 RUN pip install -r requirements.txt
 
+RUN setup.py install
+
 WORKDIR /app
 
 ENV MESSAGE "HelloWorld is running..."
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='HelloWorld',
+    version='0.0',
+    packages=['performance_summary','app'],
+    url='',
+    license='',
+    zip_safe=False,
+    author='mparker',
+    author_email='matthew.parker@sch.nhs.uk',
+    description='hello world example',
+    include_package_data=True
+)
